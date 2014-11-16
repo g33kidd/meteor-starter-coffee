d6a05c6e2f05b183655ac74485767a12ea5b52ef
Joshua Kidd <kidd.josh.343@gmail.com>
2014-11-15 19:16:45 -0700

    First Commit

---
+++ routes/config.js
@@ -0,0 +1,10 @@
+Router.configure({
+
+	layoutTemplate: 'layout',
+	loadingTemplate: 'loading',
+	notFoundTemplate: 'notFound',
+
+	// Usually subscribe to certain collections here.
+	// waitOn: function() {}
+
+});
