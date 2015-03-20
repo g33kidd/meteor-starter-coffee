e7e71da6785bafca2b73983c88a1b72901c6d180
Joshua Kidd <kidd.josh.343@gmail.com>
2015-03-20 11:28:51 -0600

    cleanup the UI helpers and added pluralize helper

--- client/config/helpers.js
+++ client/config/helpers.js
@@ -1,7 +1,12 @@
 UI.registerHelper('equals', function(a, b) {
-    if a === b {
-        true
-    }else { 
-        false
+    if(a === b){
+        return true;
+    }else {
+        return false;
     }
+});
+
+UI.registerHelper('pluralize', function(number, string) {
+  plural = string + 's';
+  return (number == 1 ? string : plural);
 });
